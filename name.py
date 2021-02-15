423a9b73c109a78a1c5293f14b72086950b84f3b
FuelDoks <fueldoks@icloud.com>
2021-02-15 17:32:18 +0300

    Конкатенация и Добавление Методов

--- name.py
+++ name.py
@@ -4,6 +4,13 @@
 
 # print('Hello,'+ " " + full_name.title() + "!")
 
-favorite_language = "Python"
-favorite_language = favorite_language.rstrip()
-print(favorite_language)
+# favorite_language = "Python"
+# favorite_language = favorite_language.rstrip()
+# print(favorite_language)
+
+# name = "Johny"
+# print('Hello, ' + name + " would you like to learn some Python today?")
+
+# name = "Oda Nobu"
+# print(name.title(),name.upper(),name.lower())
+
