a78536dfcbb1992ea8fc4f0f6fa2275593d7c166
FuelDoks <fueldoks@icloud.com>
2021-02-15 16:49:13 +0300

    Удаление Лишних пробелов с помощью метода

--- name.py
+++ name.py
@@ -1,5 +1,9 @@
-first_name = "ada"
-last_name = "lovelance"
-full_name = first_name + " " + last_name
+# first_name = "ada"
+# last_name = "lovelance"
+# full_name = first_name + " " + last_name
 
-print('Hello,'+ " " + full_name.title() + "!")
+# print('Hello,'+ " " + full_name.title() + "!")
+
+favorite_language = "Python"
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
