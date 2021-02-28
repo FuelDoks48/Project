9c1e2acce0f2da5676ae733e6ed1924c6f865a0b
FuelDoks <fueldoks@icloud.com>
2021-02-28 17:31:10 +0300

    Новый файл с циклами for

---
+++ magicians.py
@@ -0,0 +1,21 @@
+# magicians = ['alice', 'James', 'Scott', 'George']
+
+# for magician in magicians:
+#     print(magician)
+
+# ________________________________________________________________________
+
+# magicians = ['alice', 'James', 'Scott', 'George']
+# for magician in magicians:
+    # print(magician.title() +  " ,That was a great trick!")
+# ________________________________________________________________________
+
+
+# magicians = ['alice', 'James', 'Scott', 'George']
+# for magician in magicians:
+#     print(magician.title() +  " ,That was a great trick!")
+#     print("I can't wait to see your next trick, " + magician.title() + ".\n")
+# print('Thanks you, everyone. That was a magic Show!')
+# ________________________________________________________________________
+
+
