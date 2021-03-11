d7ca582349164dd20157a822b2b3572b6c0673ef
FuelDoks <fueldoks@icloud.com>
2021-03-11 17:46:57 +0300

    new lesson

---
+++ command_if.py
@@ -0,0 +1,9 @@
+
+# Обработка специальных значений
+
+# requsted_topping = ['mushrooms', 'green peppers', 'extra chesse']
+
+# for requsted_topping in requsted_topping:
+#     print (f'Adding {requsted_topping}.')
+# print(f'Finished make your pizza!')
+# __________________________________________________________
