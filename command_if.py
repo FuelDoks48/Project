55ebf174e934837c0f088c74ff63c94fca4babf5
FuelDoks <fueldoks@icloud.com>
2021-03-12 19:18:18 +0300

    цикл if

--- command_if.py
+++ command_if.py
@@ -7,3 +7,41 @@
 #     print (f'Adding {requsted_topping}.')
 # print(f'Finished make your pizza!')
 # __________________________________________________________
+# 
+
+# Проверка вхождений 
+
+# banned_user = ['marie', 'clare', 'holly', 'Robby']
+# user = 'Jannet'
+
+# if user not in banned_user:
+#     print(f'{user.title()}, you cant post a response if you wish')
+
+# __________________________________________________________
+
+# Простые команды if else 
+
+
+# age = 18 
+
+# if age >= 18:
+#     print('You are ready old')
+# else:
+#     print('Sorry, you are too young to vote.')
+
+
+# __________________________________________________________
+
+# Цепочки if-elif-else
+
+# age = 12 
+
+# if age < 5:
+#     print('Your admission cost is 0$')
+# elif age < 18:
+#     print('Your admission cost is 18$')
+# else:
+#     print('Your admission cost is 25$')
+
+
+
