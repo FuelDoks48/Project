1d5bdf57861c970bb6336887e03b9d6cc5b8b3af
FuelDoks <fueldoks@icloud.com>
2021-03-24 16:08:25 +0300

    Task for import function

---
+++ print_models.py
@@ -0,0 +1,7 @@
+import printing_function as pf
+
+pf.unprinted_designs = ["ass", 'dasd', 'asdsd', 'dsad']
+pf.completed_models = []
+
+pf.print_models(pf.unprinted_designs, pf.completed_models)
+pf.show_comleted_models(pf.completed_models)
